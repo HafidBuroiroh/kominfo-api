cd69dfb100ac7a29031a3f5bc680df5d8e09952b
HafidBuroiroh <hafidburoiroh09@gmail.com>
2024-04-04 13:00:33 +0700

    test

--- app/Http/Controllers/api/UserApiController.php
+++ app/Http/Controllers/api/UserApiController.php
@@ -37,67 +37,72 @@ class UserApiController extends Controller
  * )
  */
     public function user(Request $request){
-        $curYear = date('Y');
-        $user = DB::table('users')
+
+        $user = DB::table('form_submissions')
+            ->leftJoin('users', 'form_submissions.id_user', '=', 'users.id')
+            ->leftJoin('profil_user', 'users.id', '=', 'profil_user.id_user')
+            ->leftJoin('forms', 'form_submissions.form_id', '=', 'forms.id')
+            ->select('users.email', 'users.no_wa', 'profil_user.*', 'form_submissions.*')
+            ->whereNotNull('profil_user.jenis_kelamin')
             ->paginate(10);
 
-        // if (count($user) > 0) {
-        //     $form_id = $user[0]->form_id; // Use id_submit instead of form_id
-        //     $logic = DB::table('m_logic_level')->where('id_form', $form_id)->where('aktif', 1)->get();
-        // } else {
-        //     $logic = '';
-        //     $level = '';
-        // }
-        // foreach ($user as $value) {
-        //     if ($logic != null or $logic != '') {
-        //         $arr_level = [];
-        //         $data_submission = json_decode($value->data, true);
-        //         if ($value->data == '{}') {
-        //           continue;
-        //         }
-        //         foreach ($logic as $data_logic) {
-        //             $arr_logic = json_decode($data_logic->logic, true);
-        //             $expectedLevel = $data_logic->id_level;
-        //             foreach ($arr_logic as $formula) {
-        //                 if ($formula['parameter'] == 'false') {
-        //                     if($data_submission[$formula['input_id']] == null || $data_submission[$formula['input_id']] == ''){
-        //                         $arr_level[] = $expectedLevel;
-        //                     }else{}
-        //                 }elseif ($formula['parameter'] == 'true') {
-        //                     if($data_submission[$formula['input_id']] != null || $data_submission[$formula['input_id']] != ''){
-        //                       if(array_key_exists("val-param", $formula)){
-        //                         if ($data_submission[$formula['input_id']] == $formula['val-param']) {
-        //                           $arr_level[] = $expectedLevel;
-        //                         }
-        //                       }else{
-        //                         $arr_level[] = $expectedLevel;
-        //                       }
-        //                     }else{}
-        //                 }else{
-        //                 }
-        //             }
-        //         }
-        //         $arr_level = array_unique($arr_level);
-        //         sort($arr_level);
-        //         if (in_array(1, $arr_level) && in_array(2, $arr_level) && in_array(3, $arr_level) && in_array(4, $arr_level) ) {
-        //             $level = 'Leader';
-        //         }elseif (in_array(1, $arr_level) && in_array(2, $arr_level) && in_array(3, $arr_level)) {
-        //             $level = 'Adopter';
-        //         }
-        //         elseif (in_array(1, $arr_level) && in_array(2, $arr_level)) {
-        //             $level = 'Observer';
-        //         }
-        //         elseif (in_array(1, $arr_level)) {
-        //             $level = 'Beginner';
-        //         }else{
-        //             $level = 'Novice'; 
-        //         }
-        //         // dd(in_array([1,2], $arr_level));
-        //     }
-        //     $value->id_level = implode(', ', $arr_level);  
-        //     $value->level = $level;  
-        //     unset($value->data, $value->logic, $value->id_level, $value->import);
-        // }
+        if (count($user) > 0) {
+            $form_id = $user[0]->form_id; // Use id_submit instead of form_id
+            $logic = DB::table('m_logic_level')->where('id_form', $form_id)->where('aktif', 1)->get();
+        } else {
+            $logic = '';
+            $level = '';
+        }
+        foreach ($user as $value) {
+            if ($logic != null or $logic != '') {
+                $arr_level = [];
+                $data_submission = json_decode($value->data, true);
+                if ($value->data == '{}') {
+                  continue;
+                }
+                foreach ($logic as $data_logic) {
+                    $arr_logic = json_decode($data_logic->logic, true);
+                    $expectedLevel = $data_logic->id_level;
+                    foreach ($arr_logic as $formula) {
+                        if ($formula['parameter'] == 'false') {
+                            if($data_submission[$formula['input_id']] == null || $data_submission[$formula['input_id']] == ''){
+                                $arr_level[] = $expectedLevel;
+                            }else{}
+                        }elseif ($formula['parameter'] == 'true') {
+                            if($data_submission[$formula['input_id']] != null || $data_submission[$formula['input_id']] != ''){
+                              if(array_key_exists("val-param", $formula)){
+                                if ($data_submission[$formula['input_id']] == $formula['val-param']) {
+                                  $arr_level[] = $expectedLevel;
+                                }
+                              }else{
+                                $arr_level[] = $expectedLevel;
+                              }
+                            }else{}
+                        }else{
+                        }
+                    }
+                }
+                $arr_level = array_unique($arr_level);
+                sort($arr_level);
+                if (in_array(1, $arr_level) && in_array(2, $arr_level) && in_array(3, $arr_level) && in_array(4, $arr_level) ) {
+                    $level = 'Leader';
+                }elseif (in_array(1, $arr_level) && in_array(2, $arr_level) && in_array(3, $arr_level)) {
+                    $level = 'Adopter';
+                }
+                elseif (in_array(1, $arr_level) && in_array(2, $arr_level)) {
+                    $level = 'Observer';
+                }
+                elseif (in_array(1, $arr_level)) {
+                    $level = 'Beginner';
+                }else{
+                    $level = 'Novice'; 
+                }
+                // dd(in_array([1,2], $arr_level));
+            }
+            $value->id_level = implode(', ', $arr_level);  
+            $value->level = $level;  
+            unset($value->data, $value->logic, $value->id_level, $value->import);
+        }
 
         return response()->json($user);
     }
@@ -130,9 +135,9 @@ class UserApiController extends Controller
  */
     public function userdetail($id){
         $userdetail = DB::table('form_submissions')
-            ->Join('users', 'form_submissions.id_user', '=', 'users.id')
-            ->Join('profil_user', 'users.id', '=', 'profil_user.id_user')
-            ->Join('forms', 'form_submissions.form_id', '=', 'forms.id')
+            ->leftJoin('users', 'form_submissions.id_user', '=', 'users.id')
+            ->leftJoin('profil_user', 'users.id', '=', 'profil_user.id_user')
+            ->leftJoin('forms', 'form_submissions.form_id', '=', 'forms.id')
             ->select('users.email', 'users.no_wa', 'profil_user.*', 'form_submissions.*')
             ->whereNotNull('profil_user.jenis_kelamin')
             ->where('form_submissions.id_user', $id)
