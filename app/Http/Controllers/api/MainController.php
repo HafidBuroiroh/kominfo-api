a3f0d072c640af32d744617bdc865a4e62003b96
HafidBuroiroh <hafidburoiroh09@gmail.com>
2024-04-30 14:45:55 +0700

    fix

--- app/Http/Controllers/api/MainController.php
+++ app/Http/Controllers/api/MainController.php
@@ -39,27 +39,79 @@ class MainController extends Controller
     }
 
     public function levelumkm(){
-        $beginner = User::with('level')->wherehas('level', function($query){
-            $query->where('level', 'LIKE', '%Beginner%');
-        })->count();
+        $user = DB::table('form_submissions')
+    ->leftJoin('users', 'form_submissions.id_user', '=', 'users.id')
+    ->leftJoin('profil_user', 'users.id', '=', 'profil_user.id_user')
+    ->leftJoin('forms', 'form_submissions.form_id', '=', 'forms.id')
+    ->select('users.email', 'users.no_wa', 'profil_user.*', 'form_submissions.*')
+    ->whereNotNull('profil_user.jenis_kelamin')
+    ->paginate(10);
 
-        $observer = User::with('level')->wherehas('level', function($query){
-            $query->where('level', 'LIKE', '%Observer%');
-        })->count();
+$userLevelsCount = [
+    'Leader' => 0,
+    'Adopter' => 0,
+    'Observer' => 0,
+    'Beginner' => 0,
+    'Novice' => 0,
+];
 
-        $adopter = User::with('level')->wherehas('level', function($query){
-            $query->where('level', 'LIKE', '%Adopter%');
-        })->count();
+if (count($user) > 0) {
+    $form_id = $user[0]->form_id; // Use id_submit instead of form_id
+    $logic = DB::table('m_logic_level')->where('id_form', $form_id)->where('aktif', 1)->get();
+} else {
+    $logic = '';
+    $level = '';
+}
 
-        $leader = User::with('level')->wherehas('level', function($query){
-            $query->where('level', 'LIKE', '%Leader%');
-        })->count();
+foreach ($user as $value) {
+    if ($logic != null or $logic != '') {
+        $arr_level = [];
+        $data_submission = json_decode($value->data, true);
+        if ($value->data == '{}') {
+            continue;
+        }
+        foreach ($logic as $data_logic) {
+            $arr_logic = json_decode($data_logic->logic, true);
+            $expectedLevel = $data_logic->id_level;
+            foreach ($arr_logic as $formula) {
+                if ($formula['parameter'] == 'false') {
+                    if ($data_submission[$formula['input_id']] == null || $data_submission[$formula['input_id']] == '') {
+                        $arr_level[] = $expectedLevel;
+                    }
+                } elseif ($formula['parameter'] == 'true') {
+                    if ($data_submission[$formula['input_id']] != null || $data_submission[$formula['input_id']] != '') {
+                        if (array_key_exists("val-param", $formula)) {
+                            if ($data_submission[$formula['input_id']] == $formula['val-param']) {
+                                $arr_level[] = $expectedLevel;
+                            }
+                        } else {
+                            $arr_level[] = $expectedLevel;
+                        }
+                    }
+                }
+            }
+        }
+        $arr_level = array_unique($arr_level);
+        sort($arr_level);
+        if (in_array(1, $arr_level) && in_array(2, $arr_level) && in_array(3, $arr_level) && in_array(4, $arr_level)) {
+            $level = 'Leader';
+        } elseif (in_array(1, $arr_level) && in_array(2, $arr_level) && in_array(3, $arr_level)) {
+            $level = 'Adopter';
+        } elseif (in_array(1, $arr_level) && in_array(2, $arr_level)) {
+            $level = 'Observer';
+        } elseif (in_array(1, $arr_level)) {
+            $level = 'Beginner';
+        } else {
+            $level = 'Novice';
+        }
+        $value->id_level = implode(', ', $arr_level);
+        $value->level = $level;
+        $userLevelsCount[$level]++;
+        unset($value->data, $value->logic, $value->id_level, $value->import);
+    }
+}
 
-        $novice = User::with('level')->wherehas('level', function($query){
-            $query->where('level', 'LIKE', '%Novice%');
-        })->count();
-
-        return response()->json(['beginner'=>$beginner, 'observer'=>$observer, 'adopter'=>$adopter, 'leader'=>$leader, 'novice'=>$novice]);
+        return response()->json(['levelcount'=>$userLevelsCount]);
     }
 
     public function adopsiteknologi(){
